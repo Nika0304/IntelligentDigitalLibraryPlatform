a6603ebbdca41fa756f599f0e14ae94d63e742fe
alexandra <elena.tanase@student.upt.ro>
2026-04-26 16:21:45 +0300

    added classes & enums

--- src/main/java/com/library/model/NotificationType.java
+++ src/main/java/com/library/model/NotificationType.java
@@ -1,4 +1,4 @@
 package com.library.model;
 
-public enum Notification {
+public enum NotificationType {
 }
--- src/main/java/com/library/model/RoleType.java
+++ src/main/java/com/library/model/RoleType.java
@@ -1,4 +1,4 @@
 package com.library.model;
 
-public enum Role {
+public enum RoleType {
 }
